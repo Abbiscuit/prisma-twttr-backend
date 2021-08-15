144e8307d768d5a22d3300949ea5084b4a1ee49e
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2021-08-16 00:14:17 +0900

    Add: sample tweets

---
+++ src/routes/tweets/tweets.controller.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client';
+import { Request, Response } from 'express';
+
+const prisma = new PrismaClient();
+
+export const httpGetAllTweets = async (req: Request, res: Response) => {
+  const tweets = await prisma.tweet.findMany({
+    where: {
+      published: true,
+    },
+  });
+
+  return res.status(200).json(tweets);
+};
+
+export const httpGetTweetById = async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  const tweet = await prisma.tweet.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!tweet) {
+    return res.status(400).json({ message: 'tweet not found' });
+  }
+
+  return res.status(200).json(tweet);
+};
--- src/routes/tweets/tweets.router.ts
+++ src/routes/tweets/tweets.router.ts
@@ -1,3 +1,9 @@
-const tweetsRouter = () => {};
+import express from 'express';
+import { httpGetAllTweets, httpGetTweetById } from './tweets.controller';
+
+const tweetsRouter = express.Router();
+
+tweetsRouter.get('/', httpGetAllTweets);
+tweetsRouter.get('/:id', httpGetTweetById);
 
 export { tweetsRouter };
